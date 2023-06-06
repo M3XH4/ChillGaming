3a655ca8a4807756eda05b6cb9f27c5c340809e7
M3XH4 <119267005+M3XH4@users.noreply.github.com>
2023-06-06 08:33:48 +0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function redirectTicTacToe() {
+    location.href = "gameTicTacToe.html";
+}
+function redirectSnake() {
+    location.href = "";
+}
+function redirectTetris() {
+    location.href = "";
+}
